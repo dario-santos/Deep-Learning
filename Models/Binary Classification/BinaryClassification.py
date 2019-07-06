5d40dbe82f0861475db30ddcab523a2ed9df3218
Dário Santos <dariovfsantos@gmail.com>
2019-07-06 21:17:31 +0000

    Added a binary classification model

---
+++ Models/Binary Classification/BinaryClassification.py
@@ -0,0 +1,60 @@
+"""
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+def vectorize_sequences(sequences, dimension=10000):
+    results = np.zeros((len(sequences), dimension))
+
+    for i, sequence in enumerate(sequences):
+        results[i, sequence] = 1.0
+
+    return results
+
+
+# 1 - Loads the data
+imdb = tf.keras.datasets.imdb
+
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+
+
+# 2 - Processing of the raw data to attend the requisites of our network
+x_train = vectorize_sequences(train_data)
+x_test = vectorize_sequences(test_data)
+
+y_train = np.asarray(train_labels).astype('float32')
+y_test = np.asarray(test_labels).astype('float32')
+
+
+# 3 - The construction of our model
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Dense(32, activation='relu', input_shape=(10000, )),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+
+# 4 - Otimizador, loss function and metrics
+model.compile(optimizer='rmsprop',
+            loss='binary_crossentropy',
+            metrics=['accuracy'])
+
+
+# 5 - Train and validation
+history = model.fit(x_train, y_train, epochs=4, batch_size=512, validation_data=(x_test, y_test))
+
+# 6 - a chart to visualize our model
+history_dict = history.history
+loss_values = history_dict['loss']
+val_loss_values = history_dict['val_loss']
+epochs = range(1, len(loss_values) + 1)
+
+plt.plot(epochs, loss_values, 'bo', label='Training loss')
+plt.plot(epochs, val_loss_values, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
